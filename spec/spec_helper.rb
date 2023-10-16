6258cbdee8afca59a57c3fd3103df6abe5e25f74
Rick Bradley <rick@rickbradley.com>
2023-10-16 18:15:46 -0500

    Backport Hash#except for use in spec suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
 # frozen_string_literal: true
 
-# Spec helper
+class Hash
+  def except(*keys)
+    if keys.size > 4 && size > 4 # index if O(m*n) is big
+      h = {}
+      keys.each { |key| h[key] = true }
+      keys = h
+    end
+    reject { |key, _value| keys.include? key}
+  end
+end unless Hash.method_defined? :except
