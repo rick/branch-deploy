64f968eb0194114f3566b0bffc7f65eb906d2c81
Rick Bradley <rick@rickbradley.com>
2023-10-16 16:55:27 -0500

    First spec for CLI, trivial red->green implementation
    also, kill now-unneeded minitest_spec spec

---
+++ lib/bd_command.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Command-line interface
+
+STDERR.puts "Usage: bd [options]"
+
+exit 1
---
+++ spec/cli_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'open3'
+require_relative './spec_helper'
+
+def bd_command
+  File.expand_path(File.join(__dir__, "../bin/bd"))
+end
+
+def run_bd_command(args = "")
+  if args.empty?
+    stdout, stderr, status = Open3.capture3(bd_command)
+  else
+    stdout, stderr, status = Open3.capture3(bd_command, args)
+  end
+
+  return stdout, stderr, status.exitstatus
+end
+
+describe "CLI usage" do
+  it "should exit 1 when no arguments are passed" do
+    stdout, stderr, status = run_bd_command
+
+    _(status).must_equal 1
+    _(stdout).must_equal ""
+    _(stderr).must_match(/Usage: .*bd \[options\]/)
+  end
+end
