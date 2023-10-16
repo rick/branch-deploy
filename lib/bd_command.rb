62571d6e7d00720e10a5d6e773f28e4b4c98fa35
Rick Bradley <rick@rickbradley.com>
2023-10-16 17:59:22 -0500

    Fix a bunch of rubocop advisories

--- lib/bd_command.rb
+++ lib/bd_command.rb
@@ -4,38 +4,39 @@ require 'optparse'
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: bd [options]"
-  opts.on("-r", "--repo REPO", "Repository passed to 'git clone' command on deployment host [REQUIRED]") do |r|
+  opts.banner = 'Usage: bd [options]'
+  opts.on('-r', '--repo REPO', "Repository passed to 'git clone' command on deployment host [REQUIRED]") do |r|
     options[:repo] = r
   end
-  opts.on("-b", "--branch BRANCH", "Branch name to deploy [REQUIRED]") do |b|
+  opts.on('-b', '--branch BRANCH', 'Branch name to deploy [REQUIRED]') do |b|
     options[:branch] = b
   end
-  opts.on("-h", "--host HOSTNAME", "Target host for remote deployment [REQUIRED, or --local]") do |h|
+  opts.on('-h', '--host HOSTNAME', 'Target host for remote deployment [REQUIRED, or --local]') do |h|
     options[:host] = h
   end
-  opts.on("-l", "--local", "Deploy to the local host (default: false) [REQUIRED, or --host]") do |l|
+  opts.on('-l', '--local', 'Deploy to the local host (default: false) [REQUIRED, or --host]') do |l|
     options[:local] = l
   end
-  opts.on("-p", "--path PATH", "Deployment path on target host [REQUIRED]") do |p|
+  opts.on('-p', '--path PATH', 'Deployment path on target host [REQUIRED]') do |p|
     options[:path] = p
   end
-  opts.on("-d", "--diff", "Return diff output for proposed changes, do not deploy (default: true)") do |d|
+  opts.on('-d', '--diff', 'Return diff output for proposed changes, do not deploy (default: true)') do |d|
     options[:diff] = d
   end
-  opts.on("-c", "--confirm", "Confirm deployment -- actually update files (default: false)") do |c|
+  opts.on('-c', '--confirm', 'Confirm deployment -- actually update files (default: false)') do |c|
     options[:confirm] = c
   end
-  opts.on("-s", "--ssh-options OPTIONS", "Flags passed to the 'ssh' command for remote deployment (default: \"\")") do |s|
+  opts.on('-s', '--ssh-options OPTIONS',
+          "Flags passed to the 'ssh' command for remote deployment (default: \"\")") do |s|
     options[:ssh_options] = s
   end
-  opts.on("-t", "--temp-path", "Base path for tempdir creation on deployment host (default: OS default)") do |t|
+  opts.on('-t', '--temp-path', 'Base path for tempdir creation on deployment host (default: OS default)') do |t|
     options[:temp_path] = t
   end
-  opts.on("-v", "--verbose", "Run verbosely") do |v|
+  opts.on('-v', '--verbose', 'Run verbosely') do |v|
     options[:verbose] = v
   end
-  opts.on("-h", "--help", "Prints this help") do
+  opts.on('-h', '--help', 'Prints this help') do
     puts opts
     exit
   end
@@ -44,10 +45,7 @@ OptionParser.new do |opts|
 end.parse!
 
 unless options[:repo] && options[:branch] && options[:host] && options[:path]
-  STDERR.puts "Requires --repo"
-  STDERR.puts options[:help]
+  warn 'Requires --repo'
+  warn options[:help]
   exit 1
 end
-
-
-
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -6,7 +6,7 @@ require 'open3'
 require_relative './spec_helper'
 
 def bd_command
-  File.expand_path(File.join(__dir__, "../bin/bd"))
+  File.expand_path(File.join(__dir__, '../bin/bd'))
 end
 
 def run_bd_command(args = [])
@@ -16,43 +16,43 @@ def run_bd_command(args = [])
     stdout, stderr, status = Open3.capture3(bd_command, *args)
   end
 
-  return stdout, stderr, status.exitstatus
+  [stdout, stderr, status.exitstatus]
 end
 
 def options_full
   {
-    :repo => ["--repo", "git@github.com:rick/branch-deploy.git"],
-    :branch => ["--branch", "main"],
-    :host => ["--host", "example.com"],
-    :local => ["--local"],
-    :path => ["--path", "/var/www/branch-deploy"],
-    :diff => ["--diff"],
-    :confirm => ["--confirm"],
-    :ssh_options => ["--ssh-options", "-p 2222"],
-    :temp_path => ["--temp-path", "/tmp"],
-    :verbose => ["--verbose"],
-    :help => ["--help"]
+    repo: ['--repo', 'git@github.com:rick/branch-deploy.git'],
+    branch: ['--branch', 'main'],
+    host: ['--host', 'example.com'],
+    local: ['--local'],
+    path: ['--path', '/var/www/branch-deploy'],
+    diff: ['--diff'],
+    confirm: ['--confirm'],
+    ssh_options: ['--ssh-options', '-p 2222'],
+    temp_path: ['--temp-path', '/tmp'],
+    verbose: ['--verbose'],
+    help: ['--help']
   }
 end
 
 def options_brief
   {
-    :repo => ["-r", "git@github.com:rick/branch-deploy.git"],
-    :branch => ["-b", "main"],
-    :host => ["-h", "example.com"],
-    :local => ["-l"],
-    :path => ["-p", "/var/www/branch-deploy"],
-    :diff => ["-d"],
-    :confirm => ["-c"],
-    :ssh_options => ["-s", "-p 2222"],
-    :temp_path => ["-t", "/tmp"],
-    :verbose => ["-v"],
-    :help => ["-h"]
+    repo: ['-r', 'git@github.com:rick/branch-deploy.git'],
+    branch: ['-b', 'main'],
+    host: ['-h', 'example.com'],
+    local: ['-l'],
+    path: ['-p', '/var/www/branch-deploy'],
+    diff: ['-d'],
+    confirm: ['-c'],
+    ssh_options: ['-s', '-p 2222'],
+    temp_path: ['-t', '/tmp'],
+    verbose: ['-v'],
+    help: ['-h']
   }
 end
 
 def required_option_keys
-  [:repo, :branch, :host, :path]
+  %i[repo branch host path]
 end
 
 def args_hash(keys)
@@ -71,100 +71,100 @@ end
 # TODO: once we get something actually working internally, we will want a way to only validate arguments
 #       otherwise we will trigger deploys, etc., while just testing args
 
-describe "CLI usage" do
+describe 'CLI usage' do
   before do
     @valid_args_hash = args_hash(required_option_keys)
   end
 
-  it "should exit 1 when no arguments are passed" do
-    stdout, stderr, status = run_bd_command
+  it 'should exit 1 when no arguments are passed' do
+    _, _, status = run_bd_command
     _(status).must_equal 1
   end
 
-  it "should provide usage output when no arguments are passed" do
-    stdout, stderr, status = run_bd_command
+  it 'should provide usage output when no arguments are passed' do
+    _, stderr, = run_bd_command
     _(stderr).must_match(/Usage: .*bd \[options\]/)
   end
 
-  it "should return 0 if valid arguments are passed" do
-    stdout, stderr, status = run_bd_command(args_list(@valid_args_hash))
+  it 'should return 0 if valid arguments are passed' do
+    _, _, status = run_bd_command(args_list(@valid_args_hash))
     _(status).must_equal 0
   end
 
-  it "should not provide usage output if valid arguments are passed" do
-    stdout, stderr, status = run_bd_command(args_list(@valid_args_hash))
+  it 'should not provide usage output if valid arguments are passed' do
+    _, stderr, = run_bd_command(args_list(@valid_args_hash))
     _(stderr).must_be_empty
   end
 
-  it "should work if short repo argument is passed" do
+  it 'should work if short repo argument is passed' do
     args = to_short_arg(@valid_args_hash, :repo)
-    stdout, stderr, status = run_bd_command(args_list(args))
+    _, _, status = run_bd_command(args_list(args))
     _(status).must_equal 0
   end
 
-  it "should return 1 if no repo is passed" do
+  it 'should return 1 if no repo is passed' do
     args = args_hash(required_option_keys - [:repo])
-    stdout, stderr, status = run_bd_command(args_list(args))
+    _, _, status = run_bd_command(args_list(args))
     _(status).must_equal 1
   end
 
-  it "should provide usage output if no repo is passed" do
+  it 'should provide usage output if no repo is passed' do
     args = args_hash(required_option_keys - [:repo])
-    stdout, stderr, status = run_bd_command(args_list(args))
+    _, stderr, = run_bd_command(args_list(args))
     _(stderr).must_match(/Usage: .*bd \[options\]/)
   end
 
-  it "should work if short branch argument is passed" do
+  it 'should work if short branch argument is passed' do
     args = to_short_arg(@valid_args_hash, :branch)
-    stdout, stderr, status = run_bd_command(args_list(args))
+    _, _, status = run_bd_command(args_list(args))
     _(status).must_equal 0
   end
 
-  it "should return 1 if no branch is passed" do
+  it 'should return 1 if no branch is passed' do
     args = args_hash(required_option_keys - [:branch])
-    stdout, stderr, status = run_bd_command(args_list(args))
+    _, _, status = run_bd_command(args_list(args))
     _(status).must_equal 1
   end
 
-  it "should provide usage output if no branch is passed" do
+  it 'should provide usage output if no branch is passed' do
     args = args_hash(required_option_keys - [:branch])
-    stdout, stderr, status = run_bd_command(args_list(args))
+    _, stderr, = run_bd_command(args_list(args))
     _(stderr).must_match(/Usage: .*bd \[options\]/)
   end
 
-  it "should work if short host argument is passed" do
+  it 'should work if short host argument is passed' do
     args = to_short_arg(@valid_args_hash, :host)
-    stdout, stderr, status = run_bd_command(args_list(args))
+    _, _, status = run_bd_command(args_list(args))
     _(status).must_equal 0
   end
 
-  it "should return 1 if no host is passed" do
+  it 'should return 1 if no host is passed' do
     args = args_hash(required_option_keys - [:host])
-    stdout, stderr, status = run_bd_command(args_list(args))
+    _, _, status = run_bd_command(args_list(args))
     _(status).must_equal 1
   end
 
-  it "should provide usage output if no host is passed" do
+  it 'should provide usage output if no host is passed' do
     args = args_hash(required_option_keys - [:host])
-    stdout, stderr, status = run_bd_command(args_list(args))
+    _, stderr, = run_bd_command(args_list(args))
     _(stderr).must_match(/Usage: .*bd \[options\]/)
   end
 
-  it "should work if short path argument is passed" do
+  it 'should work if short path argument is passed' do
     args = to_short_arg(@valid_args_hash, :path)
-    stdout, stderr, status = run_bd_command(args_list(args))
+    _, _, status = run_bd_command(args_list(args))
     _(status).must_equal 0
   end
 
-  it "should return 1 if no path is passed" do
+  it 'should return 1 if no path is passed' do
     args = args_hash(required_option_keys - [:path])
-    stdout, stderr, status = run_bd_command(args_list(args))
+    _, _, status = run_bd_command(args_list(args))
     _(status).must_equal 1
   end
 
-  it "should provide usage output if no path is passed" do
+  it 'should provide usage output if no path is passed' do
     args = args_hash(required_option_keys - [:path])
-    stdout, stderr, status = run_bd_command(args_list(args))
+    _, stderr, = run_bd_command(args_list(args))
     _(stderr).must_match(/Usage: .*bd \[options\]/)
   end
 end
