5fbdfd38e70a2e21d7298b29d7c1fff849363d9c
Rick Bradley <rick@rickbradley.com>
2023-10-16 17:16:16 -0500

    Force options parsing via specs

--- lib/bd_command.rb
+++ lib/bd_command.rb
@@ -1,7 +1,52 @@
 # frozen_string_literal: true
 
-# Command-line interface
+require 'optparse'
 
-STDERR.puts "Usage: bd [options]"
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: bd [options]"
+  opts.on("-r", "--repo REPO", "Repository passed to 'git clone' command on deployment host [REQUIRED]") do |r|
+    options[:repo] = r
+  end
+  opts.on("-b", "--branch BRANCH", "Branch name to deploy [REQUIRED]") do |b|
+    options[:branch] = b
+  end
+  opts.on("-h", "--host HOSTNAME", "Target host for remote deployment [REQUIRED, or --local]") do |h|
+    options[:host] = h
+  end
+  opts.on("-l", "--local", "Deploy to the local host (default: false) [REQUIRED, or --host]") do |l|
+    options[:local] = l
+  end
+  opts.on("-p", "--path PATH", "Deployment path on target host [REQUIRED]") do |p|
+    options[:path] = p
+  end
+  opts.on("-d", "--diff", "Return diff output for proposed changes, do not deploy (default: true)") do |d|
+    options[:diff] = d
+  end
+  opts.on("-c", "--confirm", "Confirm deployment -- actually update files (default: false)") do |c|
+    options[:confirm] = c
+  end
+  opts.on("-s", "--ssh-options OPTIONS", "Flags passed to the 'ssh' command for remote deployment (default: \"\")") do |s|
+    options[:ssh_options] = s
+  end
+  opts.on("-t", "--temp-path", "Base path for tempdir creation on deployment host (default: OS default)") do |t|
+    options[:temp_path] = t
+  end
+  opts.on("-v", "--verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+  opts.on("-h", "--help", "Prints this help") do
+    puts opts
+    exit
+  end
 
-exit 1
+  options[:help] = opts.help
+end.parse!
+
+
+unless options[:repo]
+  STDERR.puts "Requires --repo"
+  STDERR.puts options[:help]
+  exit 1
+end
+
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -8,17 +8,72 @@ def bd_command
   File.expand_path(File.join(__dir__, "../bin/bd"))
 end
 
-def run_bd_command(args = "")
+def run_bd_command(args = [])
   if args.empty?
     stdout, stderr, status = Open3.capture3(bd_command)
   else
-    stdout, stderr, status = Open3.capture3(bd_command, args)
+    stdout, stderr, status = Open3.capture3(bd_command, *args)
   end
 
   return stdout, stderr, status.exitstatus
 end
 
+def options_full
+  {
+    :repo => ["--repo", "git@github.com:rick/branch-deploy.git"],
+    :branch => ["--branch", "main"],
+    :host => ["--host", "example.com"],
+    :local => ["--local"],
+    :path => ["--path", "/var/www/branch-deploy"],
+    :diff => ["--diff"],
+    :confirm => ["--confirm"],
+    :ssh_options => ["--ssh-options", "-p 2222"],
+    :temp_path => ["--temp-path", "/tmp"],
+    :verbose => ["--verbose"],
+    :help => ["--help"]
+  }
+end
+
+def options_brief
+  {
+    :repo => ["-r", "git@github.com:rick/branch-deploy.git"],
+    :branch => ["-b", "main"],
+    :host => ["-h", "example.com"],
+    :local => ["-l"],
+    :path => ["-p", "/var/www/branch-deploy"],
+    :diff => ["-d"],
+    :confirm => ["-c"],
+    :ssh_options => ["-s", "-p 2222"],
+    :temp_path => ["-t", "/tmp"],
+    :verbose => ["-v"],
+    :help => ["-h"]
+  }
+end
+
+def required_option_keys
+  [:repo, :branch, :host, :path]
+end
+
+def args_hash(keys)
+  keys.map { |k| options_full[k] }.to_h
+end
+
+def args_list(hash_args)
+  hash_args.keys.sort.map { |k| [k, hash_args[k]] }.flatten
+end
+
+def to_short_arg(hash_args, key)
+  hash_args.except(key).merge(options_brief[key])
+end
+
+# TODO: once we get something actually working internally, we will want a way to only validate arguments
+#       otherwise we will trigger deploys, etc., while just testing args
+
 describe "CLI usage" do
+  before do
+    @valid_args_hash = args_hash(required_option_keys)
+  end
+
   it "should exit 1 when no arguments are passed" do
     stdout, stderr, status = run_bd_command
     _(status).must_equal 1
@@ -30,4 +85,13 @@ describe "CLI usage" do
     _(stderr).must_match(/Usage: .*bd \[options\]/)
   end
 
+  it "should return 0 if valid arguments are passed" do
+    stdout, stderr, status = run_bd_command(args_list(@valid_args_hash))
+    _(status).must_equal 0
+  end
+
+  it "should not provide usage output if valid arguments are passed" do
+    stdout, stderr, status = run_bd_command(args_list(@valid_args_hash))
+    _(stderr).must_be_empty
+  end
 end
